40976bb575d262741211ac08d8a986a81a46fd56
Vishal Kulkarni <vishalsdk@gmail.com>
2024-09-27 20:12:04 +0530

    Added few util functions to get account details

--- trades/utils.py
+++ trades/utils.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404, reverse, redirect, render
 from django.shortcuts import render
 from trades.forms import AccountForm
 from trades.models import AccountData
+import logging
+
+logger = logging.getLogger("trades")
 
 def get_or_set_settings(request):
     try:
@@ -20,3 +23,27 @@ def get_or_set_settings(request):
     }
 
     return render(request, "pages/settings.html", ctx)
+
+def get_capital(self):
+    try:
+        acc_data = AccountData.objects.all()[0]
+        return acc_data.Capital
+    except Exception as e:
+        logger.debug("Account details not set. Returning 0")
+        return 0
+
+def get_max_pos_size(self):
+    try:
+        acc_data = AccountData.objects.all()[0]
+        return acc_data.MaxPosSizePerTrade
+    except Exception as e:
+        logger.debug("Account details not set. Returning 0")
+        return 0
+
+def get_max_risk_per_trade(self):
+    try:
+        acc_data = AccountData.objects.all()[0]
+        return acc_data.MaxPosSizePerTrade
+    except Exception as e:
+        logger.debug("Account details not set. Returning 0")
+        return 0
