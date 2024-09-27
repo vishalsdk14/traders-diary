546ef9d6418545724f5c9f8580623bc2cb1ad568
Vishal Kulkarni <vishalsdk@gmail.com>
2024-09-27 20:23:22 +0530

    Added comment to model

--- trades/models.py
+++ trades/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+"""
+    Model to store account details
+
+    Capital                 : Initial account capital
+    MaxPosSizePerTrade      : Maximum position size percentrade per Trade
+    MaxRiskPerTrade         : Maximum risk/Stop loss percentage per trade 
+"""
 class AccountData(models.Model):
     Capital             = models.IntegerField(default=1000000)      # Capital
     MaxPosSizePerTrade  = models.FloatField(default=20)  # Max Position size per trade
-    MaxRiskPerTrade     = models.FloatField(default=0.5)    # Max Risk Per trade.
+    MaxRiskPerTrade     = models.FloatField(default=5)    # Max Risk Per trade.
